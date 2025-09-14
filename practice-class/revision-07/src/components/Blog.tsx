560a9399f3dbcc5bab5639331f5cfed915f72548
aabdullahtamim <atwebofficial@gmail.com>
2025-09-14 15:06:21 +0600

    Revision-07

---
+++ practice-class/revision-07/src/components/Blog.tsx
@@ -0,0 +1,48 @@
+
+
+// export default function Blog() {
+
+import Button from "./Button";
+
+
+
+//     // const myArr = new Array(7).fill(undefined);
+
+//     // const list = myArr.map(i => <li>one</li>);
+//     // const list = myArr.forEach(i => <li>one</li>)
+//     // button.addEventListener('click', () => { })
+//     // const handleClick = () => {
+//     //     console.log('clicked');
+//     // }
+//     const handleClick = (e) => {
+//         console.log(e.target);
+
+//         console.log('clicked');
+
+//     }
+
+//     // document.getElementById('hi')?.addEventListener('click', handleClick);
+
+
+
+//     return (
+
+//         <button onClick={handleClick} id="hi" >hello</button>
+
+//     )
+// }
+
+export default function Blog() {
+
+
+
+
+    return (
+        <>
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+        </>
+    )
+}
