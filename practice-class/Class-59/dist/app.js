dce0ad1ca613cd42de81f73f9f39026a05e2f7dc
aabdullahtamim <atwebofficial@gmail.com>
2025-09-29 10:16:00 +0600

    Class-59

---
+++ practice-class/Class-59/dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.get('/about', (req, res) => {
+    res.send('this is about page , now this is another  ');
+});
+exports.default = app;
+//# sourceMappingURL=app.js.map
---
+++ practice-class/Class-59/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('this is about page , now this is another  ')
+})
+
+export default app;
