37f4876d0eb9ca2237bf78e2e766010a0a905013
aabdullahtamim <atwebofficial@gmail.com>
2025-09-30 12:54:10 +0600

    Class-60

---
+++ practice-class/Class-60/dist/modules/todo/todo.route.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+export const todoRouter = Router();
+todoRouter.get('/all-todos', (req, res) => {
+    // pull data from dataBase;
+    res.json([
+        {
+            id: 1,
+            todo: 'this is first todo'
+        }
+    ]);
+});
+todoRouter.post("/create-todo", (req, res) => {
+    // set body data to dataBase;
+    res.json({
+        "todo": "this is our first todo"
+    });
+});
+todoRouter.post("/delete-todo/:id", (req, res) => {
+    // delete data from database;
+    const todoId = req.params?.id;
+    console.log(todoId);
+    res.json({
+        message: "todo item delete successfully "
+    });
+});
+todoRouter.get('/test', (req, res) => {
+    let params = req.query;
+    console.log(params);
+    res.json({
+        message: "successfully"
+    });
+});
+//# sourceMappingURL=todo.route.js.map
