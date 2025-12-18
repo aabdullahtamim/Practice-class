24d5a4d6301f0599bd84a0bd6b4233580e3dabd6
aabdullahtamim <atwebofficial@gmail.com>
2025-12-18 20:03:45 +0600

    final project commit-6 mobile navigation real data update

---
+++ practice-class/Final-project/class-102/components/layout/public/header.tsx
@@ -0,0 +1,29 @@
+import PublicMainMenu from "@/components/public/mainMenu";
+import Navbar from "@/components/public/navbar";
+import { MapPin } from "lucide-react";
+import Link from "next/link";
+
+function PublicHeader() {
+    return (
+        <>
+            <div className="bg-primary p-2">
+                <div className="body-container">
+                    <div className="flex flex-col md:flex-row justify-between items-center">
+                        <Link className="flex text-white" href={"/"}>
+                            <MapPin />
+                            Track Order
+                        </Link>
+                        <div className="flex gap-x-3 text-white">
+                            <a className="border-r pr-3" href="tel:01604515250">01604515250</a>
+                            <Link href={"/"}>Login</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <PublicMainMenu />
+            <Navbar />
+        </>
+    )
+}
+
+export default PublicHeader;
